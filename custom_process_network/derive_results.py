fc321c9e653da00fea8a37c29ccb1515ee75b30d
moumouris <antonis.moumouris@webits.gr>
2024-06-20 12:11:28 +0300

    feat: Add file to derive network results

--- custom_process_network/derive_results.py
+++ custom_process_network/derive_results.py
@@ -1,7 +1,7 @@
 import pickle as pkl
 import pandas as pd
 
-with open('results.pkl', 'rb') as f:
+with open('custom_process_network/results.pkl', 'rb') as f:
     results = pkl.load(f)
 
 models = ['M1', 'M2', 'M3']
@@ -13,7 +13,11 @@ for model in models:
   for Dt in results.keys():  
     model_results[model]['Dt'].append(Dt)
     for key, value in results[Dt][model].items():
-      model_results[model][key].append("{:.3f}".format(value))
+      if isinstance(value, str):
+         model_results[model][key].append(value)
+      else:
+        model_results[model][key].append("{:.3f}".format(value))
 
   model_table = pd.DataFrame(model_results[model])
   model_table.to_csv('data/custom-network/' + model + '.csv', index=False)
+print(model_table)
